45d1eec3c09050f8b4dae905e272ba285acc16cb
chesspeto <fuegodoro@gmail.com>
2024-11-30 20:45:37 +0100

    añadiendo URL

--- scan.sh
+++ scan.sh
@@ -5,15 +5,21 @@ echo "introduce min-rate:"
 read MINRATE
 echo "nombre del informe"
 read INFORME
+URL=$(hostname -I)
 
-ruta=$(hostname -I)
+#nmap
 nmap -sSCV -n -Pn -vvv -p- --open $IP --min-rate=$MINRATE -oX escaneo.xml
+
+#convert html
 xsltproc escaneo.xml -o escaneo.html
+
+#clean
 mv ./escaneo.html  "$INFORME"
 rm ./escaneo.xml
 
 echo "el informe se llama $INFORME"
-echo "los puedes descargar en http://$ruta" 
+echo "los puedes descargar en http://$URL" 
 echo "pulsa CTRL+C para terminar"
+
 python3 -m http.server 80 2>/dev/null
 
